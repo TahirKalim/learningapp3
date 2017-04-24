a485621754c0c4fa347357682efbaa9e0c3f19c6
TahirKalim <thrkalim@gmail.com>
2017-04-24 19:38:11 +0200

    4.5(1) change the route to landing_page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
  
 	get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
  
  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
