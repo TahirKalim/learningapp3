221c411a0487432c86f1962e4050efd93850a27d
TahirKalim <thrkalim@gmail.com>
2017-05-01 10:51:14 +0200

    5.4 correction first dyno error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,18 @@
 class Ability
-  class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-  end
+    if user.admin?
+     can :manage, :all
+   else
+     can :manage, User, id: user.id
+     can [:index, :show, :search], Product
+     cannot [:create], Product
 
+   end
+  end
 end
-  
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
