b5b401eebaa9acf286430563612a4cb604a0a343
TahirKalim <thrkalim@gmail.com>
2017-05-04 02:52:55 +0200

    5.7 the comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 has_many :orders
+has_many :comments
 def self.search(search_term)
 		if Rails.env.development?
 	  	Product.where("name LIKE ?", "%#{search_term}%")
@@ -7,5 +8,9 @@ def self.search(search_term)
 	  	Product.where("name ilike ?", "%#{search_term}%")
 	  end
 	end
+
+def highest_rating_comment
+  comments.rating_desc.first
+end
 
 end
