5c86a1849fac9905097d5a8b05fc5aa35484e1f6
TahirKalim <thrkalim@gmail.com>
2017-06-01 09:00:09 +0200

    6.14 advace ActionCable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
+
   validates :body, :user, :product, presence: { message: "All fields required" }
   validates :rating, numericality: { only_integer: true, less_than: 6 }
 
-  scope :rating_desc, -> { order(rating: :desc) }
-  scope :rating_asc, -> { order(rating: :asc) }
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
+
+  
 end
